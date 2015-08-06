d8252c6090ff5eefcef942eba1bf934bce821e79
Hemant Sanvaria <hsanvaria@ccistudios.com>
2015-08-06 09:50:00 -0400

    nav-scroll

--- sites/all/themes/inmotion/js/nav-scroll.js
+++ sites/all/themes/inmotion/js/nav-scroll.js
@@ -1,38 +1,35 @@
 (function($) {
-	
+  
 
-	$(function()
-	{	
-     	$("#block-taxonomy-menu-block-1 .content  ul li a").each(function (index)
-      {	 
-           var val=$(this).text().replace(/\W+/g,'');
-       		 $(this).attr("href","#"+val);        
-    	});
+  $(function()
+  {    
 
-      $("#block-taxonomy-menu-block-1 .content  ul li,#footer2 ul li").each(function (index)
-      {  
-          if($(this).find('ul').length > 0 )
-          {
-            $(this).addClass('expanded');
-          }  
 
-      });
+    $("#block-taxonomy-menu-block-1 .content  ul li a").each(function (index)
+    {  
+      var val=$(this).text().replace(/\W+/g,'');
+      $(this).attr("href","#"+val);        
+    });
 
-       $(".view-services .view-content>.item-list>ul>li").each(function (index)
-      {  
-          
-            $(this).addClass('expanded');
+    $("#block-taxonomy-menu-block-1 .content  ul li,#footer2 ul li").each(function (index)
+    {  
+      if($(this).find('ul').length > 0 )
+      {
+        $(this).addClass('expanded');
+      }  
+    });
 
-      });
+    $(".view-services .view-content>.item-list>ul>li").each(function (index)
+    {  
+      $(this).addClass('expanded');
+    });
 
-     	var offset=$("#block-taxonomy-menu-block-1").offset();  
-     	var topPadding = 15;
-    	
-		$(".view-services .views-field-name .field-content").each(function (index)	
-	 	{	 
+
+    $(".view-services .views-field-name .field-content").each(function (index)  
+    {  
         var val=$(this).text().replace(/\W+/g,'');
-        $(this).attr("id",val); 	
-		});
+        $(this).attr("id",val);   
+    });
 
     $(".view-home-services-block .views-row a,#services-symptoms .views-row a,.menu-footer-menu ul li a").each(function (index) 
     {  
@@ -42,12 +39,11 @@
         $(this).parents('.views-row').addClass(val);  
     });
 
-    var url=window.location.hash;
     $("#services-block-inner .views-row a").each(function (index) 
     { 
-      var val=$(this).text().replace(/\W+/g,'');
+        var val=$(this).text().replace(/\W+/g,'');
         $(this).attr("href","#"+val);
-      });
+    });
 
     $(".view-what-your-symptoms .views-row a,").each(function (index) 
     {  
@@ -55,12 +51,14 @@
         $(this).attr("href","/services#"+val);
     });
 
-     $("#services").parent().find('ul li > a').each(function (index) 
+    $("#services").parent().find('ul li > a').each(function (index) 
     {  
         var val=$(this).text().replace(/\W+/g,'');
         $(this).attr("href","/services#"+val);
     });
-				var lastId,
+      
+
+      var lastId,
       topMenu = $("#block-taxonomy-menu-block-1 .content ul"),
     
       menuItems = topMenu.find("a"),
@@ -77,58 +75,54 @@
 
       });
 
-       $('#block-taxonomy-menu-block-1 a[href^="#"],#services-block-inner .views-row a').on('click',function (e) 
-     {
+      $('#block-taxonomy-menu-block-1 a[href^="#"],#services-block-inner .views-row a').on('click',function (e) 
+      {
           e.preventDefault();
-
           var target = this.hash;
           var $target = $(target);
           $('html, body').stop().animate({
-              'scrollTop': $target.offset().top-260
+            'scrollTop': $target.offset().top-260
           }, 150, 'swing', function () {
-              window.location.hash = target;
+             window.location.hash = target;
           });
       });
 
-  	  $(window).scroll(function (){
-
-    
+      $(window).scroll(function ()
+      {
        var fromTop = $(this).scrollTop();
-       
-       var cur = scrollItems.map(function(){
-         if ($(this).offset().top - 290< fromTop)
-           return this;
-         
+       var cur = scrollItems.map(function()
+       {
+          if ($(this).offset().top - 290< fromTop)
+          return this;   
        });
+
        cur = cur[cur.length-1];
        var id = cur && cur.length ? cur[0].id : "";
-    if (lastId !== id) {
-       lastId = id;
-     if(menuItems.filter("[href=#"+id+"]").parents('li.expanded.pointer > ul').length< 1)
-     {
-        menuItems
-        .removeClass("active");
-        $('li.expanded').removeClass("pointer");
-        $('#services-block-inner .views-row a').parents('.views-field-name').removeClass('active'); 
+       if (lastId !== id) 
+       {
+            lastId = id;
+            if(menuItems.filter("[href=#"+id+"]").parents('li.expanded.pointer > ul').length< 1)
+            {
+                menuItems
+                .removeClass("active");
+                $('li.expanded').removeClass("pointer");
+                $('#services-block-inner .views-row a').parents('.views-field-name').removeClass('active'); 
+            }  
 
-     }  
-        menuItems.parents('li.expanded').find('>ul a')
-        .removeClass("active");
+            menuItems.parents('li.expanded').find('>ul a').removeClass("active");
 
-        menuItems.filter("[href=#"+id+"]").parents('li.expanded').addClass("pointer").find('>a').addClass('active');
+            menuItems.filter("[href=#"+id+"]").parents('li.expanded').addClass("pointer").find('>a').addClass('active');
 
-        menuItems.filter("[href=#"+id+"]").parents('li.expanded > a').addClass("pointer");
-        
-         
-        url=$('#block-taxonomy-menu-block-1 .content li.pointer > a.active').attr('href');
-        $('#services-block-inner .views-row a').filter('[href='+url+']').parents('.views-field-name').addClass('active'); 
-        
-        
+            menuItems.filter("[href=#"+id+"]").parents('li.expanded > a').addClass("pointer");
 
-        menuItems.filter("[href=#"+id+"]").parents('li.pointer > a').addClass("active");
-        menuItems.filter("[href=#"+id+"]").addClass("active");
 
-   }      
+            url=$('#block-taxonomy-menu-block-1 .content li.pointer > a.active').attr('href');
+            $('#services-block-inner .views-row a').filter('[href='+url+']').parents('.views-field-name').addClass('active'); 
+
+            menuItems.filter("[href=#"+id+"]").parents('li.pointer > a').addClass("active");
+            menuItems.filter("[href=#"+id+"]").addClass("active");
+
+        }      
 
         var length = $('#sidebar').height() - $('#block-taxonomy-menu-block-1').height() + $('#sidebar').offset().top;   
         var scroll = $(this).scrollTop();
@@ -161,8 +155,8 @@
 
             $('#block-taxonomy-menu-block-1').css({
                 'position': 'fixed',
-                'top': '260px',
-                'height': height
+                'top': '260px'
+                
             });
             $('#services-block-inner').css({
                 'position': 'fixed',
@@ -170,8 +164,7 @@
                 'top': '0px',
                 'padding-bottom': '70px'
             });
-
         }
     });
-	});
+  });
 }(jQuery));
